d40230bee7c38a7b2c417bd4ec97e8b473c651ee
unknown <dlrnjsgud322@gmail.com>
2018-03-20 20:47:38 +0900

    010 detector upload

---
+++ 010detector/010detector.srcs/sources_1/new/010detector.v
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/03/20 20:46:10
+// Design Name: 
+// Module Name: 010detector
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module detector(
+    input in,
+    input clk,
+    output out
+    );
+    
+    
+    
+endmodule
