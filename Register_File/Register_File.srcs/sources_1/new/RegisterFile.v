81279ae07b60daa36bea8732e48287a65f3c00c2
unknown <dlrnjsgud322@gmail.com>
2018-03-20 20:51:47 +0900

    upload Register File

---
+++ Register_File/Register_File.srcs/sources_1/new/RegisterFile.v
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/03/20 20:50:29
+// Design Name: 
+// Module Name: RegisterFile
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RegisterFile(
+    input [3:0] Addr1,
+    input [3:0] Addr2,
+    input [4:0] Addr3,
+    input [15:0] Data3,
+    input Write,
+    input Clock,
+    input reset_n,
+    output [15:0] Data1,
+    output [15:0] Data2
+    );
+    
+    
+    
+endmodule
