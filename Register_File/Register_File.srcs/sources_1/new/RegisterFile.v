34fe5e36ac804fae2bb5ce325232518c58e5ba39
unknown <dlrnjsgud322@gmail.com>
2018-03-20 21:52:41 +0900

    Basic implementation

--- Register_File/Register_File.srcs/sources_1/new/RegisterFile.v
+++ Register_File/Register_File.srcs/sources_1/new/RegisterFile.v
@@ -20,9 +20,9 @@
 
 
 module RegisterFile(
-    input [3:0] Addr1,
-    input [3:0] Addr2,
-    input [4:0] Addr3,
+    input [1:0] Addr1,
+    input [1:0] Addr2,
+    input [1:0] Addr3,
     input [15:0] Data3,
     input Write,
     input Clock,
@@ -30,7 +30,34 @@ module RegisterFile(
     output [15:0] Data1,
     output [15:0] Data2
     );
+    reg [15:0] Data1, Data2;
+    reg [15:0] register[0:3];
     
+    //initialize register
+    initial begin
+        
+    end
     
+    //output is always data of register which is selected
+    always @(posedge Clock) begin
+        Data1 <= register[Addr1];
+        Data2 <= register[Addr2];
+    end
     
+    //Write data at positive clock if enable is 1
+    always @(posedge Clock) begin   
+        if (Write == 1) begin
+            register[Addr3] <= Data3;
+        end
+    end
+
+    always @(reset_n) begin
+        if (reset_n == 1) begin
+            register[0] <= 0;
+            register[1] <= 0;
+            register[2] <= 0;
+            register[3] <= 0;
+        end
+    end
+
 endmodule
