9eb9d00a484d5f666110c136b986d8d919e74b82
Unknown <dlrnjsgud322@gmail.com>
2018-03-26 23:50:14 +0900

    change to moore
    follow description on ppt

--- 010detector/010detector.srcs/sources_1/new/010detector.v
+++ 010detector/010detector.srcs/sources_1/new/010detector.v
@@ -26,37 +26,37 @@ module detector(
     );
     reg out;
     reg [1:0] state;
-    reg [1:0] nextState;
+    reg [1:0] nextstate;
 
+    //initializing state
     initial begin
         state <= 0;
-        out <= 0;
     end
 
-    //my fsm diagram is in report
-    /*
-     * this fsm is mealy machine, and to simplify code, 
-     * there is no next state. (maybe need some bug testing..?)
-     */
+    //always block for state alloc
     always @(posedge clk) begin
-
-        case (state)
-        0 : begin
-            out <= 0;
-            state <= (in == 0) ? 1 : 0;
-        end
-
-        1 : begin
-            out <= 0;
-            state <= (in == 1) ? 2 : 1;
-        end
-
-        2 : begin
-            out <= (in == 0) ? 1 : 0;
-            state <= (in == 0) ? 1 : 0;
-        end
-
-        endcase
-
+        state <= nextstate;    
     end
+
+    //always block for next state
+    always @(in or state) begin
+        case (state)
+            0 : nextstate <= (in == 0) ? 1 : 0;
+            1 : nextstate <= (in == 1) ? 2 : 1;
+            2 : nextstate <= (in == 0) ? 3 : 0;
+            3 : nextstate <= (in == 0) ? 1 : 2;
+        endcase
+    end
+
+    //alwyas block for output logic
+    always @(state) begin
+        case (state) 
+            0 : out <= 0;
+            1 : out <= 0;
+            2 : out <= 0;
+            3 : out <= 1;
+            default : out <= 0;
+        endcase
+    end
+
 endmodule
